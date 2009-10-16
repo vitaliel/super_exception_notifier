add077ad2c484cd7f293530ee2b4ac4f8ce56692
Vitalie Lazu <vitalie.lazu@gmail.com>
2009-10-16 17:09:52 +0300

    Use alias_method_chain to allow app to have its own rescue_action_in_public method

--- init.rb
+++ init.rb
@@ -1,1 +1,8 @@
-require "action_mailer"
+if defined?(ActionController::Base) && !ActionController::Base.include?(ExceptionNotifiable)
+  require "action_mailer"
+  require "exception_notifier"
+  require "exception_notifiable"
+  require "exception_notifier_helper"
+
+  ActionController::Base.send(:include, ExceptionNotifiable)
+end
